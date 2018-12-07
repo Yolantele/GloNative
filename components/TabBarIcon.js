760c7494ffeda257f154598eb96df8b08550ca25
Jolanta Jasiulionyte <klpdjolanta@gmail.com>
2018-12-07 17:03:20 +0000

    Defines customButton and tabBarIcon

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import { Text } from 'react-native';
-
-import Colors from '../constants/Colors';
+import { Image, StyleSheet, View} from 'react-native';
 
 export default class TabBarIcon extends React.Component {
   render() {
+
+    let { imageHeight, image, name } = this.props
+    let imageContainer = [styles.imageContainer]
+
+    if (imageHeight){
+      imageContainer.push( {height: imageHeight} ) 
+    }
+
     return (
-      <Text>
-        icon
-      </Text>
+      <View style={styles.container}>
+        <Image 
+        source={image} 
+        resizeMode={"contain"}
+        resizeMethod={"scale"}
+        style={imageContainer}
+        alt={name + ' alt'}
+        />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: '10%'
+  },
+  imageContainer: {
+    margin: '15%',
+    height: '20%',
+    padding: '10%'
+  }
+})
--- components/designedElements/CustomButton.js
+++ components/designedElements/CustomButton.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import { TouchableOpacity, StyleSheet, Button } from 'react-native';
-
 import Styles from "../../assets/Styles";
 
 export default class CustomButon extends React.Component {
 
   render() {
     let container = [localStyles.container]
-    
-    let { title, onPress, colour, disabled } = this.props
+    let { title, onPress, colour, disabled, type} = this.props
     
     return(
       <TouchableOpacity
         style={container}
       >
         <Button 
-          style={container}
+          style={type && type === 'round' ? container.push({borderRadius: Styles.shape.roundCorner}) : container}
           title={title}
           onPress={onPress}
           color={disabled ? localStyles.disabled : colour || Styles.colours.darkText}
@@ -29,29 +27,20 @@ const localStyles = StyleSheet.create({
   container: {
     backgroundColor: Styles.colours.white,
     height: 45,
-    borderRadius: 25,
+    borderRadius: 10,
     marginTop: 10,
     marginBottom: 10,
     paddingLeft: 30,
     paddingRight: 30,
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "center"
-  },
-  containerNaked: {
-    height: 20,
-    backgroundColor: "transparent"
+    justifyContent: "center",
+    shadowColor: Styles.colours.black,
+    shadowOffset: { y: 5 },
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
   },
   disabled: {
     color: Styles.colours.greyTransparent
   },
-  title: {
-    fontWeight: "200",
-    color: Styles.colours.white,
-    fontSize: 2,
-    textAlign: "center"
-  },
-  titleNaked: {
-    color: Styles.colours.primary
-  }
 });

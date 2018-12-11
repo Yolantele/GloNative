51d6546b12e033048b8ac8fd3bcf5641825913d5
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-12-11 18:20:59 +0000

    Auth flow with amplify Cognito and userPools defined

--- App.js
+++ App.js
@@ -2,58 +2,51 @@
 
 
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Platform, StatusBar, StyleSheet, View} from 'react-native';
 import AppNavigator from './navigation/AppNavigator';
 
-export default class App extends React.Component {
-  // state = {
-  //   isLoadingComplete: false,
-  // };
+import Auth from '@aws-amplify/auth';
+import Analytics from '@aws-amplify/analytics';
+import awsconfig from './aws-exports';
+import aws_exports from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react-native';
+
+import Amplify from 'aws-amplify';
+import keys from './keys';
+
+// retrieve temporary AWS credentials and sign requests
+Auth.configure(awsconfig);
+// send analytics events to Amazon Pinpoint
+Analytics.configure(awsconfig);
+
+Amplify.configure(aws_exports);
+Amplify.configure({
+  Auth: {
+      // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+      identityPoolId: keys.identityPoolId,
+      // REQUIRED - Amazon Cognito Region
+      region: keys.poolARN,
+      // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+          // domain: '.yourdomain.com',
+      // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
+      authenticationFlowType: 'USER_PASSWORD_AUTH'
+  }
+})
+
+
+
+
+// export default class App extends React.Component {
+  class App extends React.Component {
 
   render() {
-    // if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-    //   return (
-    //     <AppLoading
-    //       startAsync={this._loadResourcesAsync}
-    //       onError={this._handleLoadingError}
-    //       onFinish={this._handleFinishLoading}
-    //     />
-    //   );
-    // } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
-      );
-    // }
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <AppNavigator />
+      </View>
+    );
   }
-
-  // _loadResourcesAsync = async () => {
-  //   return Promise.all([
-  //     Asset.loadAsync([
-  //       require('./assets/images/robot-dev.png'),
-  //       require('./assets/images/robot-prod.png'),
-  //     ]),
-  //     Font.loadAsync({
-  //       // This is the font that we are using for our tab bar
-  //       ...Icon.Ionicons.font,
-  //       // We include SpaceMono because we use it in HomeScreen.js. Feel free
-  //       // to remove this if you are not using it in your app
-  //       'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-  //     }),
-  //   ]);
-  // };
-
-  // _handleLoadingError = error => {
-  //   // In this case, you might want to report the error to your error
-  //   // reporting service, for example Sentry
-  //   console.warn(error);
-  // };
-
-//   _handleFinishLoading = () => {
-//     this.setState({ isLoadingComplete: true });
-//   };
 }
 
 const styles = StyleSheet.create({
@@ -62,3 +55,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 });
+
+export default App;
+// uncomment the next line if want to add auth layer
+// export default withAuthenticator(App, true);

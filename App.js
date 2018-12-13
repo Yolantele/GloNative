b90daf1aad5e93e39ee77f1087fe81f4dce737f2
Jolanta Jasiulionyte <klpdjolanta@gmail.com>
2018-12-13 12:18:36 +0000

    Removes Amazon Amplify and sets Redux

--- App.js
+++ App.js
@@ -1,50 +1,22 @@
 
-
+import { Provider } from 'react-redux';
+import store from './store';
 
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View} from 'react-native';
 import AppNavigator from './navigation/AppNavigator';
-
-import Auth from '@aws-amplify/auth';
-import Analytics from '@aws-amplify/analytics';
-import awsconfig from './aws-exports';
-import aws_exports from './aws-exports';
-import { withAuthenticator } from 'aws-amplify-react-native';
-
-import Amplify from 'aws-amplify';
-import keys from './keys';
-
-// retrieve temporary AWS credentials and sign requests
-Auth.configure(awsconfig);
-// send analytics events to Amazon Pinpoint
-Analytics.configure(awsconfig);
-
-Amplify.configure(aws_exports);
-Amplify.configure({
-  Auth: {
-      // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
-      identityPoolId: keys.identityPoolId,
-      // REQUIRED - Amazon Cognito Region
-      region: keys.poolARN,
-      // REQUIRED - Cookie domain (only required if cookieStorage is provided)
-          // domain: '.yourdomain.com',
-      // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
-      authenticationFlowType: 'USER_PASSWORD_AUTH'
-  }
-})
-
-
-
 
 // export default class App extends React.Component {
   class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </View>
+      </Provider>
     );
   }
 }
